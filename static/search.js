120a894fcfc9e92cce402180e926442b92e2c5bf
mbouanik <mbouanik@student.42.fr>
2023-12-16 10:22:41 -0600

    cleaned up js

--- static/search.js
+++ static/search.js
@@ -2,11 +2,9 @@ const form_list_search = document.querySelector(".forms-list-search");
 form_list_search.addEventListener("submit", (evt) => {
   evt.preventDefault();
   if (evt.target.classList.contains("follows")) {
-    console.log("DOOR", evt.target.id);
     follow_user(parseInt(evt.target.id));
     forms_list_follow(evt);
   } else if (evt.target.classList.contains("delete-msg")) {
-    console.log("MOUNTAIN", evt.target.id);
     delete_msg(parseInt(evt.target.id));
     stat_msg = document.querySelector(".stat-msg");
     if (stat_msg) {
@@ -15,7 +13,6 @@ form_list_search.addEventListener("submit", (evt) => {
     const msg = document.querySelector(`#msg${evt.target.id}`);
     msg.remove();
   } else if (evt.target.classList.contains("like-form")) {
-    console.log("HELLO WORLD ", evt.target.id);
     like_msg(parseInt(evt.target.id));
     like_form(evt);
   } else if (evt.target.classList.contains("repost-form")) {
